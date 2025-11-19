f2bcbd9789af388a491e8b04aaec77572d6c05f6
Kris Celmer <krzysztof_celmer@hotmail.com>
2025-11-19 10:07:23 +0100

    changes

--- Epoxy/preinstalled/controller1/deploy-openstack.sh
+++ Epoxy/preinstalled/controller1/deploy-openstack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Epoxy/preinstalled/controller1/deploy-openstack.sh
-# Execute the lines below in a freshly installed VM:
+# Execute the lines below in after 'kolla-bootstrap.sh' completes:
 # wget -O deploy-openstack.sh "https://raw.githubusercontent.com/kriscelmer/os-arch-inst-labs/refs/heads/main/Epoxy/preinstalled/controller1/deploy-openstack.sh"
 # bash deploy-openstack.sh
 
@@ -50,17 +50,37 @@ enable_keystone: "yes"
 enable_designate: "no"
 EOF
 
-# Create globals for Glance with file backend and Horizon
+# Create globals for Glance with Ceph
 cat << 'EOF' > /etc/kolla/globals.d/10-glance-horizon.yml
 enable_glance: "yes"
 
-# Start with simple file backend; we’ll switch to Ceph later
-glance_backend_file: "yes"
-glance_backend_ceph: "no"
+# External Ceph auth
+external_ceph_cephx_enabled: "yes"
+
+glance_backend_ceph: "yes"
+glance_backend_file: "no"
+
+# Pool/user mapping
+ceph_glance_user: "glance"
+ceph_glance_pool_name: "images"
+
 EOF
 
-# Create globals for Neutron (OVN) and Nova
+# Create globals for Nova
 cat << 'EOF' > /etc/kolla/globals.d/20-neutron-nova.yml
+enable_nova: "yes"
+enable_placement: "yes"        # placement is needed when nova is enabled
+
+nova_backend_ceph: "yes"
+
+
+ceph_nova_user: "nova"
+ceph_nova_pool_name: "vms"
+ceph_cinder_user: "cinder"
+EOF
+
+# Create globals for Neutron (OVN)
+cat << 'EOF' > /etc/kolla/globals.d/30-neutron.yml
 enable_neutron: "yes"
 
 neutron_plugin_agent: "ovn"    # already set in globals, but okay to repeat
@@ -70,16 +90,11 @@ enable_ovn: "{{ enable_neutron | bool }}"
 neutron_ovn_distributed_fip: "no"
 neutron_ovn_dhcp_agent: "no"
 neutron_enable_ovn_agent: "no"
-
-enable_nova: "yes"
-enable_placement: "yes"        # placement is needed when nova is enabled
-# Keep local storage for now
-nova_backend_ceph: "no"
 EOF
 
-# Enable Cinder and Ceph backend for Cinder, Nova and Glance
+# Enable Cinder and Ceph backend for Cinder
 
-cat << 'EOF' > /etc/kolla/globals.d/30-ceph.yml
+cat << 'EOF' > /etc/kolla/globals.d/40-cinder-ceph.yml
 enable_cinder: "yes"
 enable_cinder_backup: "yes"
 
@@ -87,32 +102,22 @@ enable_cinder_backup: "yes"
 cinder_backend_ceph: "yes"
 cinder_backup_driver: "ceph"
 
-glance_backend_ceph: "yes"
-glance_backend_file: "no"
-
-nova_backend_ceph: "yes"
-
-# External Ceph auth
-external_ceph_cephx_enabled: "yes"
-
-# Pool/user mapping (match what you set in Ceph)
-ceph_glance_user: "glance"
-ceph_glance_pool_name: "images"
-
 ceph_cinder_user: "cinder"
 ceph_cinder_pool_name: "volumes"
 ceph_cinder_backup_user: "cinder-backup"
 ceph_cinder_backup_pool_name: "backups"
 
-ceph_nova_user: "nova"
-ceph_nova_pool_name: "vms"
-
-# Align client packages with your Ceph cluster version
+# Align client packages with Ceph cluster version
 ceph_version: "squid"   # or pacific/quincy/etc
 EOF
 
+# Enable Heat
+cat << 'EOF' > /etc/kolla/globals.d/50-heat.yml
+enable_heat: "yes"
+EOF
+
 # Enable dashboards
-cat << 'EOF' > /etc/kolla/globals.d/40-dashboards.yml
+cat << 'EOF' > /etc/kolla/globals.d/90-dashboards.yml
 enable_horizon: "yes"          # already enabled, but fine
 enable_skyline: "yes"
 
@@ -151,17 +156,6 @@ pip install python-openstackclient
 export OS_CLIENT_CONFIG_FILE=/etc/kolla/clouds.yaml
 export OS_CLOUD=kolla-admin
 
-# Test basic deployment
-openstack token issue
-openstack project list
-openstack service list
-
-# Test Glance
-wget -O cirros.qcow2 https://download.cirros-cloud.net/0.6.2/cirros-0.6.2-x86_64-disk.img
-openstack image create cirros --disk-format qcow2 --container-format bare --file cirros.qcow2
-openstack image list
-
-# Test networking
 # Internal tenant network (Geneve overlay via OVN)
 openstack network create demo-net
 openstack subnet create demo-subnet --network demo-net --subnet-range 192.168.100.0/24 --dns-nameserver 8.8.8.8
@@ -169,33 +163,40 @@ openstack subnet create demo-subnet --network demo-net --subnet-range 192.168.10
 # External provider network mapped to ens4/br-ex
 openstack network create public --external --provider-network-type flat --provider-physical-network physnet1
 
+# External provider subnet with IPv4 address range borrowed from VMware Workstation VMnet8 (NAT) fixed addresses range
 VM_NAT_net_prefix=$(sudo ip -4 -o a show ens32 | awk '{print $4}' | cut -d '.' -f 1,2,3)
 openstack subnet create --network public --allocation-pool start=$VM_NAT_net_prefix.100,end=$VM_NAT_net_prefix.127 --gateway $VM_NAT_net_prefix.2 --subnet-range $VM_NAT_net_prefix.0/24 public-subnet
 
+# Router linking demo-net to provider network
 openstack router create demo-router
 openstack router set demo-router --external-gateway public
 openstack router add subnet demo-router demo-subnet
 
+# Security group to allow SSH and Ping ingres
 openstack security group create --description 'Allows ssh and ping from any host' ssh-icmp
 openstack security group rule create --ethertype IPv4 --protocol icmp --remote-ip 0.0.0.0/0 ssh-icmp
 openstack security group rule create --ethertype IPv4 --protocol tcp --dst-port 22 --remote-ip 0.0.0.0/0 ssh-icmp
 
+# Flavor to accomodate cirros Instances
 openstack flavor create --ram 512 --disk 2 --vcpus 1 m1.tiny
 openstack keypair create demo-key > demo-key.pem
 chmod 600 demo-key.pem
 
-openstack server create demo-vm --flavor m1.tiny --image cirros --key-name demo-key --network demo-net --security-group ssh-icmp
-
-FIP=$(openstack floating ip create public -f value -c floating_ip_address)
-openstack server add floating ip demo-vm $FIP
-
-openstack volume type create ceph-volume-type
-openstack volume type set ceph-volume-type --property volume_backend_name=rbd1
-
-openstack volume create --size 1 --type ceph-volume-type --image cirros boot-vol
-openstack server create vm-from-volume --flavor m1.tiny --volume boot-vol --network demo-net --key-name demo-key --security-group ssh-icmp
+# Create Cirros image
+wget -O cirros.qcow2 https://download.cirros-cloud.net/0.6.3/cirros-0.6.3-x86_64-disk.img
+openstack image create cirros --disk-format qcow2 --container-format bare --file cirros.qcow2
 
 cat << 'EOF'
+
+Deployment complete.
+
+Horizon at http://10.0.0.11/
+Skyline at http://10.0.0.11:9999/
+User: admin
+Password: openstack
+
+Set the variables below to enable 'admin' user identity in OpenStack CLI:
+
 export OS_CLIENT_CONFIG_FILE=/etc/kolla/clouds.yaml
 export OS_CLOUD=kolla-admin
 EOF
--- Epoxy/preinstalled/controller1/post-deploy-validate.sh
+++ Epoxy/preinstalled/controller1/post-deploy-validate.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
 # Epoxy/preinstalled/controller1/post-deploy-validate.sh
-set -euo pipefail
+# Execute the lines below in after 'deploy-openstack.sh' completes:
+# wget -O post-deploy-validate.sh "https://raw.githubusercontent.com/kriscelmer/os-arch-inst-labs/refs/heads/main/Epoxy/preinstalled/controller1/post-deploy-validate.sh"
+# bash post-deploy-validate.sh
+
+set -e
+set -x
+
+
+export OS_CLIENT_CONFIG_FILE=/etc/kolla/clouds.yaml
+export OS_CLOUD=kolla-admin
+
+# Test basic deployment
+openstack token issue
+openstack project list
+openstack service list
+
+# Test Glance
+openstack image list
+
+# Test Nova (with ephemeral disk)
+openstack server create demo-vm --flavor m1.tiny --image cirros --key-name demo-key --network demo-net --security-group ssh-icmp
+
+# Test Floating IP assignement
+FIP=$(openstack floating ip create public -f value -c floating_ip_address)
+openstack server add floating ip demo-vm $FIP
+
+# Test Cinder
+openstack volume type create ceph-volume-type
+openstack volume type set ceph-volume-type --property volume_backend_name=rbd1
+
+# Test Nova Boot-from-volume
+openstack volume create --size 1 --type ceph-volume-type --image cirros boot-vol
+openstack server create vm-from-volume --flavor m1.tiny --volume boot-vol --network demo-net --key-name demo-key --security-group ssh-icmp
